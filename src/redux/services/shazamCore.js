ff8b5709d14634401ad28ecd5bd049957bab95d2
Emre Cetinkaya <emre.cetkaya@gmail.com>
2023-08-21 00:31:51 -0700

    Rapid API music fetching done, Loader component done, starting error component

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,12 +1,19 @@
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': '10581b7463mshb242afa21c350d6p117f35jsn53d613d44f92',
-    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
-  }
-};
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '10581b7463mshb242afa21c350d6p117f35jsn53d613d44f92');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
