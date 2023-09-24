17e3029adc97bd52b4b0f4bf70940bbadc33e48d
Emre Cetinkaya <emre.cetkaya@gmail.com>
2023-09-24 00:48:10 -0700

    ArtistDetails done, started working on around you page

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -61,6 +61,8 @@ const TopPlay = () => {
     dispatch(playPause(true));
   };
 
+
+
   return (
     <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
       <div className="w-full flex flex-col">
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -38,7 +38,6 @@ const SongDetails = () => {
 
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
-
         <div className="mt-5">
           {songData?.sections[1].type === 'LYRICS'
             ? songData?.sections[1]?.text.map((line, i) => (
@@ -58,7 +57,6 @@ const SongDetails = () => {
         handlePauseClick={handlePauseClick}
         handlePlayClick={handlePlayClick}
       />
-
     </div>
   );
 };
